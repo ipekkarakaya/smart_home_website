8ee2651a5122e3804024d6982c55ec08ede57310
Tobias <obiii89@googlemail.com>
2018-09-14 23:14:12 +0200

    1

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
 
     var doamain = "http://raspberrypi:8080/"
 
-    $("#led_on").click(function(){
+    $("#led_living_room_on").click(function(){
        $.ajax({
-            url: doamain+"led/on",
+            url: doamain+"led1/on",
             async: false,
             dataType: "text",
             error: function(xhr, status, error){
@@ -15,9 +15,9 @@ $(document).ready(function(){
 
     });
 
-    $("#led_off").click(function(){
+    $("#led_living_room_off").click(function(){
         $.ajax({
-             url: doamain+"led/off",
+             url: doamain+"led1/off",
              async: false,
              dataType: "text",
              error: function(xhr, status, error){
@@ -27,6 +27,85 @@ $(document).ready(function(){
         });
  
      });
+
+     $("#led_kitchen_on").click(function(){
+        $.ajax({
+             url: doamain+"led2/on",
+             async: false,
+             dataType: "text",
+             error: function(xhr, status, error){
+                 alert("Server ist nicht erreichbar")
+             },
+             type: "post"
+        });
+ 
+     });
+ 
+     $("#led_kitchen_off").click(function(){
+         $.ajax({
+              url: doamain+"led2/off",
+              async: false,
+              dataType: "text",
+              error: function(xhr, status, error){
+                 alert("Server ist nicht erreichbar")
+              },
+              type: "post"
+         });
+  
+      });
+
+      $("#led_bedroom_on").click(function(){
+        $.ajax({
+             url: doamain+"led3/on",
+             async: false,
+             dataType: "text",
+             error: function(xhr, status, error){
+                 alert("Server ist nicht erreichbar")
+             },
+             type: "post"
+        });
+ 
+     });
+ 
+     $("#led_bedroom_off").click(function(){
+         $.ajax({
+              url: doamain+"led3/off",
+              async: false,
+              dataType: "text",
+              error: function(xhr, status, error){
+                 alert("Server ist nicht erreichbar")
+              },
+              type: "post"
+         });
+  
+      });
+
+      $("#led_bathroom_on").click(function(){
+        $.ajax({
+             url: doamain+"led4/on",
+             async: false,
+             dataType: "text",
+             error: function(xhr, status, error){
+                 alert("Server ist nicht erreichbar")
+             },
+             type: "post"
+        });
+ 
+     });
+ 
+     $("#led_bathroom_off").click(function(){
+         $.ajax({
+              url: doamain+"led4/off",
+              async: false,
+              dataType: "text",
+              error: function(xhr, status, error){
+                 alert("Server ist nicht erreichbar")
+              },
+              type: "post"
+         });
+  
+      });
+   
 
      $("#alarm_on").click(function(){
         $.ajax({
